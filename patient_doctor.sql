651f66cc5743d3386cff89782b7272a003f4bf20
William Franz Lamberti <wlamber2@gmu.edu>
2024-05-30 14:25:18 -0400

    Update patient_doctor.sql

--- patient_doctor.sql
+++ patient_doctor.sql
@@ -15,7 +15,23 @@ VALUES ('Alice Smith', 'Dr. Jones', 'Blue Cross'),
 	   ('Elizabeth Moore', 'Dr. Miller', 'Aetna'),
 	   ('Jack Smith', 'None', NULL);
 	   
-	   
+-- Patient cost data
+CREATE TABLE PatientYearlyCost (
+ patient_id INT PRIMARY KEY,
+ patient_name VARCHAR(50) NOT NULL,
+ year_val INT NOT NULL,
+ total_cost_usd DOUBLE NOT NULL
+);
+
+INSERT INTO PatientYearlyCost (patient_id, patient_name, year_val, total_cost_usd)
+VALUES
+ (1, 'Alice Smith', 2023, 1500.00),
+ (2, 'Bob Johnson', 2023, 2800.00),
+ (3, 'Charlie Brown', 2022, 1000.00),
+ (4, 'Diana Jones', 2023, 500.00),
+ (5, 'Elizabeth Moore', 2022, 3200.00),
+ (7, 'Liz Johnson', 2023, 0.00);
+
 -- Doctor data
 CREATE TABLE doctors (
   doctor_id INT PRIMARY KEY AUTO_INCREMENT,
