8ca5f180315d4269aaee16b47b6f726a75469935
William Franz Lamberti <wlamber2@gmu.edu>
2024-05-30 11:41:04 -0400

    Update patient_doctor.sql

--- patient_doctor.sql
+++ patient_doctor.sql
@@ -11,7 +11,9 @@ INSERT INTO patients (patient_name, doctor_saw, insurance_provider)
 VALUES ('Alice Smith', 'Dr. Jones', 'Blue Cross'),
        ('Bob Johnson', 'Dr. Lee', 'Aetna'),
        ('Charlie Brown', 'Dr. Miller', 'Cigna'),
-       ('Diana Jones', 'Dr. Williams', 'Humana');
+       ('Diana Jones', 'Dr. Williams', 'Humana'),
+	   ('Elizabeth Moore', 'Dr. Miller', 'Aetna'),
+	   ('Jack Smith', 'None', NULL);
 	   
 	   
 -- Doctor data
@@ -26,4 +28,5 @@ INSERT INTO doctors (doctor_name, specialty, accepted_insurances)
 VALUES ('Dr. Jones', 'Pediatrics', 'Blue Cross, Cigna, Humana'),
        ('Dr. Lee', 'Orthopedics', 'Aetna, Humana'),
        ('Dr. Miller', 'General Medicine', 'Blue Cross, Aetna, Cigna'),
-       ('Dr. Williams', 'Ear, Nose & Throat', 'Blue Cross, Aetna');
+       ('Dr. Williams', 'Ear, Nose & Throat', 'Blue Cross, Aetna'),
+	   ('Dr. Martin', 'Cardiology', 'Aetna, Cigna');
